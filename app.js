4adba7f42285c68051c8d95f37bfbfda9db2301f
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2021-10-17 19:03:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ function helpFunction(client, message) {
 	let helpWebUrl = 'https://github.com/GWMCwing/AuditBot/blob/master/docs/helpList.md';
 	let prefixServer = server_getGuild(client, message).prefix;
 	let msg = 'This guild uses ```' + prefixServer + '``` as prefix\n';
-	msg += `Please visit the below website for complete command list: ${helpWeb}`;
+	msg += `Please visit the below website for complete command list: ${helpWebUrl}`;
 	return message.channel.send(msg);
 }
 
