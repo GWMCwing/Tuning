ab22444b55b20498729d617ddd53e9786daa8c5e
GW_MC <ping.chan78@gmail.com>
2021-10-17 13:20:50 +0800

    Fixed typing error

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ function player_ConnectFunction(client, message) {
 	let [code, authorVC, clientVC] = checkAuthorInChannel(client, message);
 	if (code == 11) return message.channel.send('Please wait until the queue ended');
 	if (code == 10) return message.channel.send('Please enter a Voice channel');
-	consoleLogFormator(`Trying to Connect VCchannel ID: ${authorVC.id} at ${guildOBj.id}`);
+	consoleLogFormator(`Trying to Connect VCchannel ID: ${authorVC.id} at ${guildObj.id}`);
 	//! connect to author channel
 	guildObj.player.connect(message, authorVC);
 }
@@ -190,7 +190,7 @@ function player_DisconnectFunction(client, message) {
 	let [code, authorVC, clientVC] = checkAuthorInChannel(client, message);
 	if (code == 10) return message.channel.send('You are not in the same Voice Channel');
 	let guildObj = server_getGuild(client, message);
-	consoleLogFormator(`Trying to Connect VCchannel ID: ${clientVC.id} at ${guildOBj.id}`);
+	consoleLogFormator(`Trying to Connect VCchannel ID: ${clientVC.id} at ${guildObj.id}`);
 	//! disconenct
 	guildObj.player.disconnect();
 }
@@ -373,7 +373,7 @@ var commandDict = {
 client.on('messageCreate', async (message) => {
 	// consoleLogFormator('Recieved: ' + message.content);
 	if (message.author.bot) return;
-	let guildOBj = server_getGuild(client, message);
+	let guildObj = server_getGuild(client, message);
 	if (message.content[0] != PREFIX) return;
 	let firstArg = message.content.split(' ')[0].slice(1);
 	// consoleLogFormator(`Recieved: ${firstArg}`);
